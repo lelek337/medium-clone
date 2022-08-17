8a265e6e396a90f51cd75dfd22e2a9f3d097c1e8
lelek337 <ioan.af@mail.ru>
2022-08-17 07:21:10 +0300

    lesson 12

--- mediumclone-angular/src/app/auth/auth.module.ts
+++ mediumclone-angular/src/app/auth/auth.module.ts
@@ -2,7 +2,9 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { StoreModule } from "@ngrx/store";
 import { RegisterComponent } from "./register/register.component";
+import { reducers } from "./store/reducers";
 
 const routes = [
   {
@@ -14,7 +16,8 @@ const routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers)
   ],
   declarations: [
     RegisterComponent
